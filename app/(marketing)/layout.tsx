c8d38642efde264c72c5b2c7c81a455c9833573e
FalconerOne <omajonathan@gmail.com>
2025-10-19 19:31:32 +0100

    Updated layout.tsx

--- app/(marketing)/layout.tsx
+++ app/(marketing)/layout.tsx
@@ -2,15 +2,16 @@
 
 import { ReactNode } from "react";
 import AboutPromoBanner from "./about/AboutPromoBanner";
+import MarketingFooter from "./MarketingFooter";
 
 export default function MarketingLayout({ children }: { children: ReactNode }) {
   return (
     <div className="min-h-screen flex flex-col bg-gradient-to-b from-orange-50 via-white to-orange-100 text-gray-800">
-      {/* 🔝 Rotating cross-brand promo banner */}
       <AboutPromoBanner />
 
-      {/* 🌍 Page content */}
       <div className="flex-1">{children}</div>
+
+      <MarketingFooter />
     </div>
   );
 }
