1329656eb386bb48ea4657891f34eb2cbdf7e321
FalconerOne <omajonathan@gmail.com>
2025-10-21 04:11:42 +0100

    Create socials.ts

---
+++ config/socials.ts
@@ -0,0 +1,19 @@
+// /config/socials.ts
+
+export const socials = [
+  {
+    name: "Twitter",
+    url: "https://twitter.com/MyGiveAway",
+    icon: "FaTwitter",
+  },
+  {
+    name: "Facebook",
+    url: "https://facebook.com/MyGiveAway",
+    icon: "FaFacebook",
+  },
+  {
+    name: "LinkedIn",
+    url: "https://linkedin.com/company/MyGiveAway",
+    icon: "FaLinkedin",
+  },
+];
