6e7054541b96b9120c2188bc4cd64721de695665
FalconerOne <omajonathan@gmail.com>
2025-10-21 10:47:42 +0100

    Update layout.tsx

--- app/(marketing)/layout.tsx
+++ app/(marketing)/layout.tsx
@@ -1,16 +1,12 @@
 "use client";
 
 import { ReactNode } from "react";
-import AboutPromoBanner from "./about/AboutPromoBanner";
 import MarketingFooter from "./MarketingFooter";
 
 export default function MarketingLayout({ children }: { children: ReactNode }) {
   return (
     <div className="min-h-screen flex flex-col bg-gradient-to-b from-orange-50 via-white to-orange-100 text-gray-800">
-      <AboutPromoBanner />
-
       <div className="flex-1">{children}</div>
-
       <MarketingFooter />
     </div>
   );
