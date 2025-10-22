186b8ca8518f947b0ebaa23d109d6f7b60308987
FalconerOne <omajonathan@gmail.com>
2025-10-22 06:14:05 +0100

    Update input.tsx

--- components/ui/input.tsx
+++ components/ui/input.tsx
@@ -1,14 +1,17 @@
 "use client";
 
-import React from "react";
+import * as React from "react";
 import { cn } from "@/lib/utils";
 
-export const Input = React.forwardRef<HTMLInputElement, React.InputHTMLAttributes<HTMLInputElement>>(
+export interface InputProps
+  extends React.InputHTMLAttributes<HTMLInputElement> {}
+
+export const Input = React.forwardRef<HTMLInputElement, InputProps>(
   ({ className, ...props }, ref) => (
     <input
       ref={ref}
       className={cn(
-        "w-full px-3 py-2 border border-gray-300 rounded-xl focus:ring-2 focus:ring-blue-500 focus:outline-none",
+        "w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:ring-2 focus:ring-blue-500 focus:outline-none text-sm",
         className
       )}
       {...props}
