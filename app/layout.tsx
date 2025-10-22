ddb7adb11766d7cb3fa8a35dcf1d8caa856ad660
FalconerOne <omajonathan@gmail.com>
2025-10-22 06:52:56 +0100

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,10 +1,5 @@
-import "./globals.css";
 import { GlobalToastProvider } from "@/providers/GlobalToastProvider";
-
-export const metadata = {
-  title: "MyGiveAway App",
-  description: "Join, Win, and track GiveAways that Delight's You & Support's Charity.",
-};
+import "./globals.css";
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
