b7ece2e59da6877813631d4f298ac6a5fc4afd60
FalconerOne <omajonathan@gmail.com>
2025-10-24 02:08:17 +0100

    Update socials.ts

--- config/socials.ts
+++ config/socials.ts
@@ -1,19 +1,6 @@
-// /config/socials.ts
-
-export const socials = [
-  {
-    name: "Twitter",
-    url: "https://twitter.com/MyGiveAway",
-    icon: "FaTwitter",
-  },
-  {
-    name: "Facebook",
-    url: "https://facebook.com/MyGiveAway",
-    icon: "FaFacebook",
-  },
-  {
-    name: "LinkedIn",
-    url: "https://linkedin.com/company/MyGiveAway",
-    icon: "FaLinkedin",
-  },
-];
+// Auto-patched stubs for social links
+export const socialLinks = {
+  twitter: '#',
+  facebook: '#',
+  linkedin: '#'
+};
